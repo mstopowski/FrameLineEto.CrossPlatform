0f01e6c6a0f9f75081ae9809a22e62337450bb18
mstopowski <49496093+mstopowski@users.noreply.github.com>
2020-09-17 09:24:23 +0200

    Mac init

---
+++ FrameLineEto.Common/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional
+// These will show in Rhino's option dialog, in the tab Plug-ins
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+
+// Rhino requires a Guid assigned to the assembly.
+[assembly: Guid("8AFA5E59-4145-4CA9-A690-450E03622D39")]
